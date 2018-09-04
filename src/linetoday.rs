8a8b810e6c7c4d6d0008a32f9b60ac7c4e3ff902
Nurahmadie <nurahmadie@gmail.com>
2018-09-04 20:26:33 +0700

    finished xml content :tada:

--- src/linetoday.rs
+++ src/linetoday.rs
@@ -1,15 +1,15 @@
 use actix_web::{error::ErrorInternalServerError, Error, HttpResponse};
-use chrono::{offset::Utc, DateTime};
+use chrono::{offset::Utc, DateTime, Duration};
 use config::Config;
 use quick_xml::{
     events::{BytesDecl, BytesEnd, BytesStart, BytesText, Event},
     Error as XMLError, Writer,
 };
-use rss::{Channel, Item};
 use wordpress::Feed;
 
 use std::boxed::Box;
 use std::collections::hash_map::DefaultHasher;
+use std::hash::Hasher;
 
 pub struct LineToday<'a> {
     feed: Feed,
@@ -17,6 +17,7 @@ pub struct LineToday<'a> {
 }
 
 enum ChildContent<'a> {
+    CData(&'a str),
     Text(&'a str),
     Func(Box<Fn(&'a LineToday<'a>, &mut Writer<Vec<u8>>) -> Result<(), XMLError> + 'a>),
     FuncItem(
@@ -67,6 +68,9 @@ impl<'a> LineToday<'a> {
     ) -> Result<(), XMLError> {
         writer.write_event(Event::Start(BytesStart::borrowed_name(tag_name)))?;
         match content {
+            ChildContent::CData(txt) => {
+                writer.write_event(Event::CData(BytesText::from_escaped(txt.as_bytes())))?;
+            }
             ChildContent::Text(txt) => {
                 writer.write_event(Event::Text(BytesText::from_plain_str(txt)))?;
             }
@@ -106,63 +110,62 @@ impl<'a> LineToday<'a> {
 
     fn id_for(&self, idx: usize) -> String {
         let channel = self.feed.borrow_channel();
-        let item = channel.items()[idx];
-        let indexed_title = String::new();
-        let hasher = DefaultHasher::new();
+        let item = &channel.items()[idx];
+        let mut indexed_title = String::new();
+        let mut hasher = DefaultHasher::new();
 
-        hasher.write(item.link().unwrap_or_else(|| {
-            indexed_title.push_str(idx.to_string());
-            indexed_title.push_str(channel.title());
-            indexed_title
-        }));
-        hasher.finish().to_string();
+        hasher.write(
+            item.link()
+                .unwrap_or_else(|| {
+                    indexed_title.push_str(&idx.to_string());
+                    indexed_title.push_str(&channel.title());
+                    &indexed_title
+                })
+                .as_bytes(),
+        );
+        hasher.finish().to_string()
     }
 
     fn language(&self) -> String {
         let channel = self.feed.borrow_channel();
+        let lang = self.config.line_lang.clone();
 
-        self.config
-            .line_lang
-            .unwrap_or_else(|| channel.language().unwrap_or("en").chars().take(2).collect())
+        lang.unwrap_or_else(|| channel.language().unwrap_or("en").chars().take(2).collect())
     }
 
     fn start_pub_date(&self, idx: usize) -> String {
         let channel = self.feed.borrow_channel();
-        let item = channel.items()[idx];
+        let item = &channel.items()[idx];
 
         self.to_unix_time_millis(item.pub_date().unwrap_or(""))
     }
 
     fn end_pub_date(&self, idx: usize) -> String {
         let channel = self.feed.borrow_channel();
-        let item = channel.items()[idx];
+        let item = &channel.items()[idx];
 
         let end_date = self.to_datetime(item.pub_date().unwrap_or(""));
-        ((end_date + ::time::Duration::days(14)).timestamp() * 1000).to_string()
+        ((end_date + Duration::days(14)).timestamp() * 1000).to_string()
     }
 
     fn article_xml(&'a self, writer: &mut Writer<Vec<u8>>, idx: usize) -> Result<(), XMLError> {
         let article_id = self.id_for(idx);
         let language = self.language();
-        let native_country = self.config.line_native_country;
+        let native_country = &self.config.line_native_country;
         let start_pub = self.start_pub_date(idx);
         let end_pub = self.end_pub_date(idx);
         let channel = self.feed.borrow_channel();
-        let item = channel.items()[idx];
+        let item = &channel.items()[idx];
         let title = item.title().unwrap_or(channel.title());
         let category = item.categories()[0].name();
         let author = item.author().unwrap_or(channel.title());
         let source = item.link().unwrap_or(channel.link());
 
         self.xml_tag(writer, b"ID", ChildContent::Text(&article_id))?;
-        self.xml_tag(
-            writer,
-            b"nativeCountry",
-            ChildContent::Text(&native_country),
-        )?;
+        self.xml_tag(writer, b"nativeCountry", ChildContent::Text(native_country))?;
         self.xml_tag(writer, b"language", ChildContent::Text(&language))?;
 
-        if Some(pub_cnt) = self.config.line_pub_to_country {
+        if let Some(ref _p) = self.config.line_pub_to_country {
             self.xml_tag(
                 writer,
                 b"publishCountries",
@@ -170,7 +173,7 @@ impl<'a> LineToday<'a> {
             )?;
         }
 
-        if Some(excl_cnt) = self.config.line_excl_from_country {
+        if let Some(ref _e) = self.config.line_excl_from_country {
             self.xml_tag(
                 writer,
                 b"excludedCountries",
@@ -189,20 +192,71 @@ impl<'a> LineToday<'a> {
         self.xml_tag(
             writer,
             b"contents",
-            childContent::FuncItem(Box::new(Self::content_xml), idx),
+            ChildContent::FuncItem(Box::new(Self::content_xml), idx),
         )?;
 
-        self.xml_tag(writer, b"author", childContent::Text(&author))?;
-        self.xml_tag(writer, b"sourceUrl", childContent::Text(&source))?;
+        self.xml_tag(writer, b"author", ChildContent::Text(&author))?;
+        self.xml_tag(writer, b"sourceUrl", ChildContent::Text(&source))?;
 
         Ok(())
     }
 
-    fn content_xml(&'a self, writer: &mut Writer<Vec<u8>>, idx: usize) -> Result<(), XMLError> {}
+    fn content_xml(&'a self, writer: &mut Writer<Vec<u8>>, idx: usize) -> Result<(), XMLError> {
+        self.xml_tag(
+            writer,
+            b"text",
+            ChildContent::FuncItem(Box::new(Self::content_text_xml), idx),
+        )?;
+        Ok(())
+    }
 
-    fn publish_to_country_xml(&'a self, writer: &mut Writer<Vec<u8>>) -> Result<(), XMLError> {}
+    fn content_text_xml(
+        &'a self,
+        writer: &mut Writer<Vec<u8>>,
+        idx: usize,
+    ) -> Result<(), XMLError> {
+        let channel = self.feed.borrow_channel();
+        let item = &channel.items()[idx];
+        let content = item
+            .content()
+            .unwrap_or_else(|| item.description().unwrap_or("(No Content)"));
 
-    fn exclude_from_country_xml(&'a self, writer: &mut Writer<Vec<u8>>) -> Result<(), XMLError> {}
+        self.xml_tag(writer, b"content", ChildContent::CData(content))?;
+        Ok(())
+    }
+
+    fn country_xml(
+        &'a self,
+        writer: &mut Writer<Vec<u8>>,
+        countries: &str,
+    ) -> Result<(), XMLError> {
+        for country in countries.split(',') {
+            self.xml_tag(writer, b"country", ChildContent::Text(country))?;
+        }
+        Ok(())
+    }
+
+    fn publish_to_country_xml(&'a self, writer: &mut Writer<Vec<u8>>) -> Result<(), XMLError> {
+        let countries = &self
+            .config
+            .line_pub_to_country
+            .clone()
+            .unwrap_or(String::new());
+
+        self.country_xml(writer, countries)?;
+        Ok(())
+    }
+
+    fn exclude_from_country_xml(&'a self, writer: &mut Writer<Vec<u8>>) -> Result<(), XMLError> {
+        let countries = &self
+            .config
+            .line_excl_from_country
+            .clone()
+            .unwrap_or(String::new());
+
+        self.country_xml(writer, countries)?;
+        Ok(())
+    }
 }
 
 pub fn from<'l>(feed: Feed, config: &'l Config) -> LineToday {
